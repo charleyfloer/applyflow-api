49b272d4fe1b53d151c7c36ff5b4e8b4c92122d7
charleyfloer <charley.floer001@gmail.com>
2026-09-02 13:27:57 -0400

    feat(filters-pagination): add filtering, search, ordering and global pagination

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'users',
     'companies',
@@ -147,6 +148,10 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
     ),
+    "DEFAULT_PAGINATION_CLASS": (
+        "rest_framework.pagination.PageNumberPagination",
+    ),
+    "PAGE_SIZE": 5,
 }
 
 from datetime import timedelta
